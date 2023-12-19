933cedd6e1d7a589c011f8f591046d67b2226304
MohamedFawzy <mfawzy22@gmail.com>
2023-12-20 00:39:29 +0400

    add dense vector search utility function

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -106,37 +106,69 @@ def umap_plot_old(sentences, emb):
     )
     return chart
 
+
 def print_result(result):
     """ Print results with colorful formatting """
-    for i,item in enumerate(result):
+    for i, item in enumerate(result):
         print(f'item {i}')
         for key in item.keys():
             print(f"{key}:{item.get(key)}")
             print()
         print()
 
-### Revised version
-def keyword_search(query, 
+# Revised version
+
+
+def keyword_search(query,
                    client,
-                   results_lang='en', 
-                   properties = ["title","url","text"],
+                   results_lang='en',
+                   properties=["title", "url", "text"],
                    num_results=3):
 
     where_filter = {
-    "path": ["lang"],
-    "operator": "Equal",
-    "valueString": results_lang
+        "path": ["lang"],
+        "operator": "Equal",
+        "valueString": results_lang
     }
 
     response = (
         client.query.get("Articles", properties)
         .with_bm25(
-          query=query
+            query=query
         )
         .with_where(where_filter)
         .with_limit(num_results)
         .do()
-        )
-    
+    )
+
     result = response['data']['Get']['Articles']
+    return result
+
+
+def dense_retrieval(query,
+                    client,
+                    results_lang='en',
+                    properties=["text", "title", "url", "views",
+                                "lang", "_additional {distance}"],
+                    num_results=5):
+
+    nearText = {"concepts": [query]}
+
+    # To filter by language
+    where_filter = {
+        "path": ["lang"],
+        "operator": "Equal",
+        "valueString": results_lang
+    }
+    response = (
+        client.query
+        .get("Articles", properties)
+        .with_near_text(nearText)
+        .with_where(where_filter)
+        .with_limit(num_results)
+        .do()
+    )
+
+    result = response['data']['Get']['Articles']
+
     return result
